83e9b0866bf83c9267bb5bbe41ba1a4590f41722
vnthanh25 <vnthanh25@gmail.com>
2015-04-03 14:11:58 +0700

    DetectFaceCamera: Ver 1

--- Face/vntface/vntface/Test/DetectFaceCamera.cpp
+++ Face/vntface/vntface/Test/DetectFaceCamera.cpp
@@ -1,43 +1,95 @@
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
+#include<opencv2/opencv.hpp> 
+#include<opencv2/highgui/highgui.hpp>
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+using namespace cv;
 
-// Function main
-int main(void)
-{
-	std::string face_cascade_name = "cascades/haarcascade_frontalface_alt.xml";
-	cv::CascadeClassifier face_cascade;
-	//// Load the cascade
-	if (!face_cascade.load(face_cascade_name))
-	{
-		printf("--(!)Error loading\n");
-		return (-1);
-	};
+CascadeClassifier faceCade;
 
-	//// Read the image file
-	cv::Mat image;
-	cv::Mat image_gray;
-	std::vector<cv::Rect> faces;
-	cv::VideoCapture cap(0);
-	while (1)
-	{
-		bool hasFrame = cap.read(image);
-		if (hasFrame)
+String faceCascadeName = "cascades/lbpcascade_frontalface.xml";
+String FaceDetectWindow = "Face Detector Window";
+String FaceDetectGrayWindow = "Face Detector Gray Window";
+
+
+int main() {
+
+	VideoCapture cap(0);
+	Mat camFrames, grayFrames;
+	vector<Rect> faces;
+	long imageIndex = 0;
+
+	int preX = 0, preY = 0;
+	int j = 0;
+
+	if (!faceCade.load(faceCascadeName)){ cout << "--(!)Error loading\n"; return -1; };
+
+	while (1) {
+		cap >> camFrames;
+
+		cvtColor(camFrames, grayFrames, CV_BGR2GRAY);
+		equalizeHist(grayFrames, grayFrames);
+
+		faceCade.detectMultiScale(grayFrames, faces, 1.1, 2, 0, Size(160, 160));
+
+		for (int i = 0; i < faces.size(); i++)
 		{
-			cv::cvtColor(image, image_gray, CV_BGR2GRAY);
-			cv::equalizeHist(image_gray, image_gray);
-			// Detect faces
-			face_cascade.detectMultiScale(image_gray, faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+			Mat faceROI = grayFrames(faces[i]);
 
-			for (size_t i = 0; i < faces.size(); i++)
+			rectangle(camFrames, Rect(faces[i].x - 25, faces[i].y - 25, faces[i].width + 35, faces[i].height + 35), Scalar(0, 0, 255), 1, 1, 0);
+
+			Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
+
+
+			cout << "\nx = " << faces[i].x << "\ty = " << faces[i].y;
+
+			//while(j==10)
+			//{
+			if ((faces[i].y<125) && (faces[i].x<255))
 			{
-				cv::rectangle(image, faces[i], cv::Scalar(0, 0, 255));
+				printf("  RU");
+				system("xte 'mousermove 10 -10'");
 			}
-			cv::imshow("image", image);
-			if (cv::waitKey(30) >= 0) break;
+			else if ((faces[i].y>185) && (faces[i].x<255)){
+				printf("  RD");
+				system("xte 'mousermove 10 10'");
+			}
+			else if ((faces[i].y>185) && (faces[i].x>305)){
+				printf("  LD");
+				system("xte 'mousermove -10 10'");
+			}
+			else if ((faces[i].y<125) && (faces[i].x<255)){
+				printf("  LU");
+				system("xte 'mousermove -10 -10'");
+			}
+			else if ((faces[i].y>185) && (faces[i].x>255) && (faces[i].x<305)){
+				printf("  Down");
+				system("xte 'mousermove 0 10'");
+			}
+			else if ((faces[i].y<125) && (faces[i].x>255) && (faces[i].x<305)){
+				printf("  up");
+				system("xte 'mousermove 0 -10'");
+			}
+			else if ((faces[i].y>125) && (faces[i].y<185) && (faces[i].x<255)){
+				printf("  L");
+				system("xte 'mousermove -10 0'");
+			}
+			else if ((faces[i].y>125) && (faces[i].y<185) && (faces[i].x>305)){
+				printf("  R");
+				system("xte 'mousermove 10 0'");
+			}
+			preX = faces[i].x;
+			preY = faces[i].y;
+			j = 0;
 		}
+		//j++;
+		//}
+
+		imshow(FaceDetectWindow, camFrames);
+		imshow(FaceDetectGrayWindow, grayFrames);
+		if (waitKey(30) >= 0) break;
+
 	}
-	return 0;
+
 }
