f3a48db2af925ce240a078dc6da70dc4a91c7f58
vnthanh25 <vnthanh25@gmail.com>
2015-04-03 17:11:26 +0700

    LBPFeature: ver 2

--- Face/vntface/vntface/Test/LBPFeature.cpp
+++ Face/vntface/vntface/Test/LBPFeature.cpp
@@ -9,8 +9,11 @@ VNThanh
 #include <iostream>
 #include <string>
 
+#include "Utilities.h"
+vnt::Utilites util;
+
 //\\ Chuyen doi so thanh chuoi nhi phan.
-std::string convertUnsignedIntToBinary(unsigned int pValue)
+std::string convertUnsignedIntToBinary(int pValue)
 {
 	std::string result = "";
 	while (pValue > 0)
@@ -20,14 +23,16 @@ std::string convertUnsignedIntToBinary(unsigned int pValue)
 	}
 	return result;
 }
+
 //\\ Kiem tra chuoi nhi phan dong nhat (khi so dich chuyen tu 0->1 hoac tu 1->0 la <= 2).
 bool checkUniformPartern(const std::string pValue)
 {
-	if (pValue.length() < 2)
+	int len = pValue.length();
+	if (len < 2)
 		return false;
 	int iCount = 0;
 	char pre = pValue[0];
-	for (int i = 1; i < pValue.length(); i++)
+	for (int i = 1; i < len; i++)
 	{
 		if (pValue[i] != pre)
 		{
@@ -39,6 +44,7 @@ bool checkUniformPartern(const std::string pValue)
 	}
 	return true;
 }
+
 //\\ Bo sung ky tu "c" ve ben trai chuoi ky tu.
 std::string leftPad(const std::string s, int len, char c)
 {
@@ -49,11 +55,12 @@ std::string leftPad(const std::string s, int len, char c)
 	}
 	return result;
 }
+
 //\\ Khoi tao 58 gia tri dong nhat.
-std::vector<unsigned int> initHisVal()
+std::vector<int> initHisVal()
 {
-	std::vector<unsigned int> result;
-	for (unsigned int i = 0; i < 256; i++)
+	std::vector<int> result;
+	for (int i = 0; i < 256; i++)
 	{
 		std::string bin = leftPad(convertUnsignedIntToBinary(i), 8, '0');
 		if (checkUniformPartern(bin))
@@ -63,11 +70,12 @@ std::vector<unsigned int> initHisVal()
 	}
 	return result;
 }
+
 //\\ Hien thi ra man hinh dos cac gia tri dong nhat trong 256 gia tri.
 void showLBPUniValues()
 {
 	int iCount = 0;
-	for (unsigned int i = 0; i < 256; i++)
+	for (int i = 0; i < 256; i++)
 	{
 		std::string bin = leftPad(convertUnsignedIntToBinary(i), 8, '0');
 		if (checkUniformPartern(bin))
@@ -78,10 +86,11 @@ void showLBPUniValues()
 	}
 	std::cout << "Total : " + std::to_string(iCount);
 }
+
 //\\ Tinh LBP cua ma tran co bien. Tra ve ket qua ma tran k co bien. (bien = pad).
 cv::Mat LBP(const cv::Mat image, int pad)
 {
-	cv::Mat lbp(image.rows - pad * 2, image.cols - pad * 2, CV_8UC1, cv::Scalar(255));
+	cv::Mat lbp(image.rows - pad * 2, image.cols - pad * 2, CV_8UC1, cv::Scalar(0));
 	int center = 0;
 	int center_lbp = 0;
 
@@ -121,6 +130,7 @@ cv::Mat LBP(const cv::Mat image, int pad)
 	}
 	return lbp;
 }
+
 cv::Mat LBP(const std::string src_image, int pad)
 {
 	cv::Mat temp_image = cv::imread(src_image.c_str(), 1);
@@ -130,6 +140,7 @@ cv::Mat LBP(const std::string src_image, int pad)
 	cv::Mat lbp = LBP(Image, pad);
 	return lbp;
 }
+
 //\\ Chia anh thanh n x n vung.
 std::vector<cv::Mat> divImage(const cv::Mat image, int n)
 {
@@ -147,27 +158,74 @@ std::vector<cv::Mat> divImage(const cv::Mat image, int n)
 	}
 	return result;
 }
-//\\ Chia anh thanh n x n vung. Co cong them bien pad.
-std::vector<cv::Mat> divImageEx(const cv::Mat image, int n, int pad)
+
+//\\ Chia anh thanh n x n vung. Co cong them bien (pad) de tinh convolution.
+//\\ Chieu rong/cao cua anh truyen vao phai nho hon tich chieu rong/cao cua n vung con cong them 2pad.
+std::vector<cv::Mat> divImageDilation(const cv::Mat image, int n, int pad)
 {
 	std::vector<cv::Mat> result;
-	cv::Mat imageEx(image.cols + pad * 2, image.rows + pad * 2, CV_8UC1, cv::Scalar(255));
-	image.copyTo(imageEx.colRange(pad, image.cols + pad).rowRange(pad, image.rows + pad));
 	int width = image.cols / n + pad * 2;
 	int height = image.rows / n + pad * 2;
+	cv::Mat imageDil(image.cols + pad * 2, image.rows + pad * 2, CV_8UC1, cv::Scalar(0));
+	image.copyTo(imageDil.colRange(pad, image.cols + pad).rowRange(pad, image.rows + pad));
 	cv::Mat region;
-	for (int y = 0; y < imageEx.rows - height; y += height - pad * 2)
+	for (int y = 0; y < imageDil.rows - height + 1; y += height - pad * 2)
 	{
-		for (int x = 0; x < imageEx.cols - width; x += width - pad * 2)
+		for (int x = 0; x < imageDil.cols - width + 1; x += width - pad * 2)
 		{
-			region = imageEx(cv::Rect(x, y, width, height));
+			region = imageDil(cv::Rect(x, y, width, height));
+			util.writeMatUChar(region, "Txt/region" + to_string(y) + "_" + to_string(x) + ".txt");
 			result.push_back(region);
 		}
 	}
 	return result;
 }
+
+//\\ Chia anh thanh n x n vung. Co cong them bien (pad) de tinh convolution.
+//\\ Chieu rong/cao cua anh truyen vao phai lon hon tich chieu rong/cao cua n vung con cong them 2pad.
+std::vector<cv::Mat> divImageErosion(const cv::Mat image, int n, int pad)
+{
+	std::vector<cv::Mat> result;
+	//\\ Tinh chieu rong va cao cua vung can tinh.
+	int width = image.cols / n;
+	int height = image.rows / n;
+	int widthmod = image.cols - (width * n + pad * 2);
+	int heightmod = image.rows - (height * n + pad * 2);
+	cv::Mat imageEro = image(cv::Rect(widthmod / 2, heightmod / 2, width * n + pad * 2, height * n + pad * 2));
+	width += pad * 2;
+	height += pad * 2;
+	cv::Mat region;
+	for (int y = 0; y < imageEro.rows - height + 1; y += height - pad * 2)
+	{
+		for (int x = 0; x < imageEro.cols - width + 1; x += width - pad * 2)
+		{
+			region = imageEro(cv::Rect(x, y, width, height));
+			util.writeMatUChar(region, "Txt/region" + to_string(y) + "_" + to_string(x) + ".txt");
+			result.push_back(region);
+		}
+	}
+	return result;
+}
+
+//\\ Chia anh thanh n x n vung. Co cong them bien (pad) de tinh convolution.
+std::vector<cv::Mat> divImage(const cv::Mat image, int n, int pad)
+{
+	std::vector<cv::Mat> result;
+	//\\ Tinh chieu rong va cao cua vung can tinh.
+	int width = image.cols / n;
+	int height = image.rows / n;
+	int widthmod = image.cols - (width * n + pad * 2);
+	int heightmod = image.rows - (height * n + pad * 2);
+	if (widthmod < 0 || heightmod < 0)
+		result = divImageDilation(image, n, pad);
+	else
+		result = divImageErosion(image, n, pad);
+
+	return result;
+}
+
 //\\ Tinh histogram cho cac gia tri dong nhat.
-std::vector<int> hisLBPUni(const cv::Mat region, const std::vector<unsigned int> hisTemp)
+std::vector<int> hisLBPUni(const cv::Mat region, const std::vector<int> hisTemp)
 {
 	int len = hisTemp.size();//  58 muc gia tri.
 	std::vector<int> result(len + 1);// 59 muc gia tri.
@@ -175,7 +233,7 @@ std::vector<int> hisLBPUni(const cv::Mat region, const std::vector<unsigned int>
 	{
 		for (int x = 0; x < region.cols; x++)
 		{
-			unsigned int value = (unsigned int)region.at<uchar>(y, x);
+			int value = (int)region.at<uchar>(y, x);
 			//\\ Tim trong 58 gia tri.
 			bool isFound = false;
 			for (int n = 0; n < len; n++)
@@ -194,26 +252,66 @@ std::vector<int> hisLBPUni(const cv::Mat region, const std::vector<unsigned int>
 	}// for rows
 	return result;
 }
+
+cv::Mat createMat()
+{
+	cv::Mat result;
+	size_t width = 11;
+	size_t height = 11;
+	result = cv::Mat(height, width, CV_8UC1, cv::Scalar(0));
+	for (size_t y = 0; y < height; y++)
+	{
+		for (size_t x = 0; x < width; x++)
+		{
+			if (y % 2 == 1 || x % 2 == 1)
+				result.at<uchar>(y, x) = 1;
+		}
+	}
+	return result;
+}
+
+//\\ Chuyen vector<vector<in>> thanh Mat.
+cv::Mat convertV2ToMat(std::vector<std::vector<int>> lbpFeature, int width, int height)
+{
+	cv::Mat result = cv::Mat::zeros(height, width, CV_32FC1);
+	for (size_t y = 0; y < lbpFeature.size(); y++)
+	{
+		for (size_t x = 0; x < lbpFeature[y].size(); x++)
+		{
+			result.at<int>(y, x) = lbpFeature[y][x];
+		}
+	}
+	return result;
+}
+
 //\\ Ham chinh.
 int main(int argc, const char *argv[]) {
 	//\\ Doc anh.
+	////cv::Mat image = createMat();
 	cv::Mat image = cv::imread("images/lena.png", CV_8UC1);
-	//\\ Khoi tao cac gia tri dong nhat.
-	std::vector<unsigned int> hisValues = initHisVal();
+	util.writeMatUChar(image, "Txt/image.txt");
+	//\\ Khoi tao 58 gia tri dong nhat trong 256 gia tri.
+	std::vector<int> hisValues = initHisVal();
 	//\\ Ban kinh diem lan can pad = 1;
 	int pad = 1;
 	//\\ Chia anh thanh 3 x 3 vung. Co mo rong bien voi do lon la pad.
-	std::vector<cv::Mat> regions = divImageEx(image, 3, pad);
+	std::vector<cv::Mat> regions = divImage(image, 3, pad);
 	//\\ Tinh dac trung LBP voi 9 vung x 59 khoang.
 	std::vector<std::vector<int>> lbpFeature;
 	for (int i = 0; i < regions.size(); i++)
 	{
 		//\\ Tinh LBP cho moi vung.
 		cv::Mat lbp = LBP(regions[i], pad);
+		util.writeMatUChar(lbp, "Txt/lbp" + to_string(i) + ".txt");
 		//\\ Tinh histogram (59 values) cho LBP with uniform partern.
 		std::vector<int> lbphis = hisLBPUni(lbp, hisValues);
 		lbpFeature.push_back(lbphis);
 	}
+	//\\ Chuyen doi vector 2 chieu thanh Mat.
+	cv::Mat lbpFeatureMat = convertV2ToMat(lbpFeature, hisValues.size() + 1, regions.size());
+	util.writeMatInt(lbpFeatureMat, "Txt/lbpFeatureMat.txt");
+
+	showLBPUniValues();
 	cv::waitKey(0);
 	//\\cv::destroyAllWindows();
 	return 0;
