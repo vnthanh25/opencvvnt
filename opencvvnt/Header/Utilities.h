62d4e3f8adaede6e8c8e4f1feef2b13a6115c038
vnthanh25 <vnthanh25@gmail.com>
2015-04-06 18:10:22 +0700

    Them method: increaseNumber

--- opencvvnt/Header/Utilities.h
+++ opencvvnt/Header/Utilities.h
@@ -9,15 +9,19 @@ namespace vnt
 	class Utilites
 	{
 	public:
+		/********** Constructor **********/
+		Utilites();
+		~Utilites();
 
-		
+
 		RNG rng;
-		
+
 		double angle(Point pt1, Point pt2, Point pt0);
 		void setLabel(Mat& im, const string label, vector<Point>& contour);
-		Mat detectsimpleshape(Mat src);
 
+		//\\ Bo sung ky tu "c" ve ben trai chuoi ky tu.
 		string leftPad(string s, int len, char c);
+		//\\ Ghi Mat ra file.
 		void writeMatUChar(Mat img, string fname);
 		void writeMatInt(Mat img, string fname);
 		void writeMatBasic(Mat img, string fname);
@@ -25,10 +29,19 @@ namespace vnt
 
 		Mat convolutionInt(Mat img, Mat tmp);
 
-		Utilites();
-		~Utilites();
+		//\\ Chuyen doi so thanh chuoi nhi phan.
+		std::string convertUnsignedIntToBinary(int pValue);
+		//\\ Tao Mat voi gia tri radom.
+		cv::Mat createMatRandom(int width, int height);
 
-	private:
+		//\\ Chuyen vector<vector<in>> thanh Mat.
+		cv::Mat convertV2ToMat(std::vector<std::vector<int>> lbpFeature, int width, int height);
+
+		//\\ Lay so tiep theo tu chuoi so truyen vao.
+		std::string increaseNumber(const std::string pNumber, const int pIncrease);
+
+		Mat detectsimpleshape(Mat src);
+
 
 	};
 }
--- opencvvnt/Source/Utilities.cpp
+++ opencvvnt/Source/Utilities.cpp
@@ -3,13 +3,21 @@
 
 using namespace vnt;
 
+/********** Constructor **********/
+Utilites::Utilites()
+{
+}
+Utilites::~Utilites()
+{
+}
+
 double Utilites::angle(Point pt1, Point pt2, Point pt0)
 {
 	double dx1 = pt1.x - pt0.x;
 	double dy1 = pt1.y - pt0.y;
 	double dx2 = pt2.x - pt0.x;
 	double dy2 = pt2.y - pt0.y;
-	return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+	return (dx1*dx2 + dy1*dy2) / sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
 void Utilites::setLabel(Mat& im, const string label, vector<Point>& contour)
@@ -23,9 +31,178 @@ void Utilites::setLabel(Mat& im, const string label, vector<Point>& contour)
 	Rect r = boundingRect(contour);
 
 	Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
-	rectangle(im, pt + Point(0, baseline), pt + Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
-	putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
+	rectangle(im, pt + Point(0, baseline), pt + Point(text.width, -text.height), CV_RGB(255, 255, 255), CV_FILLED);
+	putText(im, label, pt, fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
 }
+
+//\\ Bo sung ky tu "c" ve ben trai chuoi ky tu.
+string Utilites::leftPad(string s, int len, char c)
+{
+	std::string result = s;
+	for (int i = s.length(); i < len; i++)
+	{
+		result = c + result;
+	}
+	return result;
+}
+//\\ Ghi Mat ra file.
+void Utilites::writeMatUChar(Mat img, string fname)
+{
+	FileStorage file(fname, FileStorage::WRITE);
+	//file << "mat" << img;
+	file << "mat" << "[:";
+	string sValue = "";
+	for (int x = 0; x < img.cols; x++)
+	{
+		stringstream out;
+		out << x;
+		string value = out.str();
+		sValue += leftPad(value, 3, ' ') + " ";
+	}
+	file << "{" << "x" << sValue + "-1" << "}";
+	for (int y = 0; y < img.rows; y++)
+	{
+		sValue = "";
+		for (int x = 0; x < img.cols; x++)
+		{
+			int iv = (int)img.at<uchar>(Point(x, y));
+			stringstream out;
+			out << iv;
+			string value = out.str();
+			sValue += leftPad(value, 3, ' ') + " ";
+		}
+		stringstream out;
+		out << y;
+		file << "{" << "y" << sValue + out.str() << "}";
+	}
+	file << "]";
+	file.release();
+}
+void Utilites::writeMatInt(Mat img, string fname)
+{
+	FileStorage file(fname, FileStorage::WRITE);
+	//file << "mat" << img;
+	file << "mat" << "[:";
+	string sValue = "";
+	for (int x = 0; x < img.cols; x++)
+	{
+		stringstream out;
+		out << x;
+		string value = out.str();
+		sValue += leftPad(value, 3, ' ') + " ";
+	}
+	file << "{" << "x" << sValue + "-1" << "}";
+	for (int y = 0; y < img.rows; y++)
+	{
+		sValue = "";
+		for (int x = 0; x < img.cols; x++)
+		{
+			int iv = img.at<int>(Point(x, y));
+			stringstream out;
+			out << iv;
+			string value = out.str();
+			sValue += leftPad(value, 3, ' ') + " ";
+		}
+		stringstream out;
+		out << y;
+		file << "{" << "y" << sValue + out.str() << "}";
+	}
+	file << "]";
+	file.release();
+}
+void Utilites::writeMatBasic(Mat img, string fname)
+{
+	FileStorage storage(fname, cv::FileStorage::WRITE);
+	storage << "image" << img;
+	storage.release();
+}
+void Utilites::readMatBasic(Mat img, string fname)
+{
+	FileStorage storage(fname, cv::FileStorage::READ);
+	storage["image"] >> img;
+	storage.release();
+}
+Mat Utilites::convolutionInt(Mat img, Mat tmp)
+{
+	Mat src = Mat::zeros(img.rows + tmp.rows - 1, img.cols + tmp.cols - 1, CV_32SC1);
+	img.copyTo(src.rowRange(0, img.rows).colRange(0, img.cols));
+	Mat dst = Mat::zeros(img.size(), CV_32SC1);
+	// Step by step of the source image.
+	for (int y = 0; y < img.rows; y++)
+	{
+		for (int x = 0; x < img.cols; x++)
+		{
+			int sum = 0;
+			// Step by step of the template.
+			for (int m = 0; m < tmp.rows; m++)
+			{
+				for (int n = 0; n < tmp.cols; n++)
+				{
+					int isrc = src.at<int>(y + m, x + n);
+					int itmp = tmp.at<int>(m, n);
+					sum += src.at<int>(y + m, x + n) * tmp.at<int>(m, n);
+				}
+			}
+			dst.at<int>(y, x) = sum;
+		}
+	}
+	return dst;
+}
+
+
+//\\ Chuyen doi so thanh chuoi nhi phan.
+std::string Utilites::convertUnsignedIntToBinary(int pValue)
+{
+	std::string result = "";
+	while (pValue > 0)
+	{
+		result = std::to_string(pValue % 2) + result;
+		pValue = pValue / 2;
+	}
+	return result;
+}
+
+//\\ Tao Mat voi gia tri radom.
+cv::Mat Utilites::createMatRandom(int width, int height)
+{
+	cv::Mat result;
+	result = cv::Mat(height, width, CV_8UC1, cv::Scalar(0));
+	for (size_t y = 0; y < height; y++)
+	{
+		for (size_t x = 0; x < width; x++)
+		{
+			if (y % 2 == 1 || x % 2 == 1)
+				result.at<uchar>(y, x) = 1;
+		}
+	}
+	return result;
+}
+
+//\\ Chuyen vector<vector<in>> thanh Mat.
+cv::Mat Utilites::convertV2ToMat(std::vector<std::vector<int>> lbpFeature, int width, int height)
+{
+	cv::Mat result = cv::Mat::zeros(height, width, CV_32FC1);
+	for (size_t y = 0; y < lbpFeature.size(); y++)
+	{
+		for (size_t x = 0; x < lbpFeature[y].size(); x++)
+		{
+			result.at<int>(y, x) = lbpFeature[y][x];
+		}
+	}
+	return result;
+}
+
+//\\ Lay so tiep theo tu chuoi so truyen vao.
+std::string Utilites::increaseNumber(const std::string pNumber, const int pIncrease)
+{
+	std::string result;
+	int len = pNumber.length();
+	int number = std::stoi(pNumber) + pIncrease;
+	result = std::to_string(number);
+	result = leftPad(result, len, '0');
+	return result;
+}
+
 
 Mat DataMat::src, DataMat::gray, DataMat::filter, DataMat::edge, DataMat::dst;
 
@@ -34,7 +211,7 @@ Mat Utilites::detectsimpleshape(Mat img)
 	//Mat gray, filter, edge, dst;
 	DataMat::src = img;
 	//filter = src.clone();
-	GaussianBlur(DataMat::src, DataMat::filter, Size(5,5), 0, 0, BORDER_DEFAULT );
+	GaussianBlur(DataMat::src, DataMat::filter, Size(5, 5), 0, 0, BORDER_DEFAULT);
 	medianBlur(DataMat::filter, DataMat::filter, 3);
 	// Convert to grayscale
 	cvtColor(DataMat::filter, DataMat::gray, CV_BGR2GRAY);
@@ -53,7 +230,7 @@ Mat Utilites::detectsimpleshape(Mat img)
 
 	for (int i = 0; i < contours.size(); i++)
 	{
-		Scalar color = Scalar(rng.uniform(0,255), rng.uniform(0, 255), rng.uniform(0, 255));
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 
 		// Approximate contour with accuracy proportional to the contour perimeter
 		approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true)*0.02, true);
@@ -64,7 +241,7 @@ Mat Utilites::detectsimpleshape(Mat img)
 
 		if (approx.size() == 3)
 		{
-			drawContours(DataMat::dst, contours, i,  color, -1);
+			drawContours(DataMat::dst, contours, i, color, -1);
 		}
 		else if (approx.size() >= 4 && approx.size() <= 6)
 		{
@@ -73,8 +250,8 @@ Mat Utilites::detectsimpleshape(Mat img)
 
 			// Get the cosines of all corners
 			vector<double> cos;
-			for (int j = 2; j < vtc+1; j++)
-				cos.push_back(angle(approx[j%vtc], approx[j-2], approx[j-1]));
+			for (int j = 2; j < vtc + 1; j++)
+				cos.push_back(angle(approx[j%vtc], approx[j - 2], approx[j - 1]));
 
 			// Sort ascending the cosine values
 			sort(cos.begin(), cos.end());
@@ -87,15 +264,15 @@ Mat Utilites::detectsimpleshape(Mat img)
 			// to determine the shape of the contour
 			if (vtc == 4 && mincos >= -0.1 && maxcos <= 0.3)
 			{
-				drawContours(DataMat::dst, contours, i,  color, -1);
+				drawContours(DataMat::dst, contours, i, color, -1);
 			}
 			else if (vtc == 5 && mincos >= -0.34 && maxcos <= -0.27)
 			{
-				drawContours(DataMat::dst, contours, i,  color, -1);
+				drawContours(DataMat::dst, contours, i, color, -1);
 			}
 			else if (vtc == 6 && mincos >= -0.55 && maxcos <= -0.45)
 			{
-				drawContours(DataMat::dst, contours, i,  color, -1);
+				drawContours(DataMat::dst, contours, i, color, -1);
 			}
 		}
 		else
@@ -106,139 +283,13 @@ Mat Utilites::detectsimpleshape(Mat img)
 			int radius = r.width / 2;
 
 			if (abs(1 - ((double)r.width / r.height)) <= 0.2 &&
-			    abs(1 - (area / (CV_PI * pow(radius, 2.0)))) <= 0.2)
+				abs(1 - (area / (CV_PI * pow(radius, 2.0)))) <= 0.2)
 			{
-				drawContours(DataMat::dst, contours, i,  color, -1);
+				drawContours(DataMat::dst, contours, i, color, -1);
 			}
 		}
 
 	}
 
 	return DataMat::dst;
-}
-
-
-
-string Utilites::leftPad(string s, int len, char c)
-{
-	for(int i = s.length(); i < len; i++)
-	{
-		s = c + s;
-	}
-	return s;
-}
-
-void Utilites::writeMatUChar(Mat img, string fname)
-{
-	FileStorage file(fname, FileStorage::WRITE);
-	//file << "mat" << img;
-	file << "mat" << "[:"; 
-	string sValue = "";
-	for(int x = 0; x < img.cols; x++)
-	{
-		stringstream out;
-		out << x;
-		string value = out.str();
-		sValue += leftPad(value, 3, ' ') + " ";
-	}
-	file << "{" << "x" << sValue + "-1" << "}";
-	for(int y = 0; y < img.rows; y++)
-	{
-		sValue = "";
-		for(int x = 0; x < img.cols; x++)
-		{
-			int iv = (int)img.at<uchar>(Point(x,y));
-			stringstream out;
-			out << iv;
-			string value = out.str();
-			sValue += leftPad(value, 3, ' ') + " ";
-		}
-		stringstream out;
-		out << y;
-		file << "{" << "y" << sValue + out.str() << "}";
-	}
-	file << "]";
-	file.release();
-}
-
-void Utilites::writeMatInt(Mat img, string fname)
-{
-	FileStorage file(fname, FileStorage::WRITE);
-	//file << "mat" << img;
-	file << "mat" << "[:"; 
-	string sValue = "";
-	for(int x = 0; x < img.cols; x++)
-	{
-		stringstream out;
-		out << x;
-		string value = out.str();
-		sValue += leftPad(value, 3, ' ') + " ";
-	}
-	file << "{" << "x" << sValue + "-1" << "}";
-	for(int y = 0; y < img.rows; y++)
-	{
-		sValue = "";
-		for(int x = 0; x < img.cols; x++)
-		{
-			int iv = img.at<int>(Point(x,y));
-			stringstream out;
-			out << iv;
-			string value = out.str();
-			sValue += leftPad(value, 3, ' ') + " ";
-		}
-		stringstream out;
-		out << y;
-		file << "{" << "y" << sValue + out.str() << "}";
-	}
-	file << "]";
-	file.release();
-}
-
-void Utilites::writeMatBasic(Mat img, string fname)
-{
-	FileStorage storage(fname, cv::FileStorage::WRITE);
-	storage << "image" << img;
-	storage.release();  
-}
-
-void Utilites::readMatBasic(Mat img, string fname)
-{
-	FileStorage storage(fname, cv::FileStorage::READ);
-	storage["image"] >> img;
-	storage.release();
-}
-
-Mat Utilites::convolutionInt(Mat img, Mat tmp)
-{
-	Mat src = Mat::zeros(img.rows + tmp.rows - 1, img.cols + tmp.cols - 1, CV_32SC1);
-	img.copyTo(src.rowRange(0, img.rows).colRange(0, img.cols));
-	Mat dst = Mat::zeros(img.size(), CV_32SC1);
-	// Step by step of the source image.
-	for (int y = 0; y < img.rows; y++)
-	{
-		for (int x = 0; x < img.cols; x++)
-		{
-			int sum = 0;
-			// Step by step of the template.
-			for (int m = 0; m < tmp.rows; m++)
-			{
-				for (int n = 0; n < tmp.cols; n++)
-				{
-					int isrc = src.at<int>(y + m, x + n);
-					int itmp = tmp.at<int>(m,n);
-					sum += src.at<int>(y + m, x + n) * tmp.at<int>(m,n);
-				}
-			}
-			dst.at<int>(y,x) = sum;
-		}
-	}
-	return dst;
-}
-
-Utilites::Utilites()
-{
-}
-
-Utilites::~Utilites()
-{
 }
